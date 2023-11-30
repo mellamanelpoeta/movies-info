e04519fbff09f96ed046b2825bcfe0ec64421b57
mellamanelpoeta <gerarm.guerrero@gmail.com>
2023-11-30 02:29:36 -0600

    2:29

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Detener y eliminar contenedores existentes (si los hay)
-docker-compose down
-
-# Construir y levantar los contenedores definidos en docker-compose.yml
 docker-compose up --build
